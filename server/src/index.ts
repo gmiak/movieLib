5a4f963de49831d9eb6f60ac8a904dfddfb7e101
gmiak <kayss.g@gmail.com>
2022-02-12 22:14:33 +0100

    Starting to separate Service layer and Router layer in movieFavorite.router.ts by dependency injection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,11 @@
 import Express from "express";
 import { favoriteMovieRouter } from "./router/favoriteMovie.router";
-import {movieRouter} from "./router/movie.router";
+import {makeDefaultMovieRouter} from "./router/movie.router";
 
 const app : Express.Express = Express();
 
-/*app.get("/", (req: Express.Request, res : Express.Response) => {
-    res.send("Hello World");
-});*/
+
 app.use(Express.json())  // Returns all i ask in json
-app.use("/movie", movieRouter);
+app.use("/movie", makeDefaultMovieRouter);
 app.use("/movie/favorite", favoriteMovieRouter);
 app.listen(8080);
